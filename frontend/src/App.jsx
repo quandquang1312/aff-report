a7c7e1a268a5fae9f515d8933dcfcd7ed43d1a7d
quandquang1312 <quangquanb96@gmail.com>
2026-06-24 16:54:25 +0700

    Update BACKEND_API_URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useMemo, useState } from "react";
 import "./App.css";
-const API_URL = import.meta.env.BACKEND_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 export default function App() {
     const [adsFile, setAdsFile] = useState(null);
