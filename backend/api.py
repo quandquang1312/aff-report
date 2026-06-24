12b5390e0679f4a4061d7225218d2bf643f1151a
quandquang1312 <quangquanb96@gmail.com>
2026-06-24 16:15:48 +0700

    add allow_origins to frontend

--- backend/api.py
+++ backend/api.py
@@ -11,7 +11,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://aff-report.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
